7625338537e936ce830c6a55f0f765ef22b3c3ff
Andrew Carr <aacarr5@gmail.com>
2015-10-15 18:42:16 -0500

    Fix assoications

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+	belongs_to :pairing
+	has_many :menus, :through => :pairing
+	has_many :restaurants, :through => :menus
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,5 @@
 class Menu < ActiveRecord::Base
-
+	belongs_to :restaurant
+	has_many :pairings
+	has_many :items, :through => :pairings
 end
