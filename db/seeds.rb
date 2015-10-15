88aee57a5c6a1fa330c037cedb8d27849cadc18c
Andrew Carr <aacarr5@gmail.com>
2015-10-15 18:42:47 -0500

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# require 'faker'
+
+first = Restaurant.create!(name: Faker::Company.name)
+
+second = Restaurant.create!(name: Faker::Company.name)
+
+restaurant_array = [first,second]
+
+food = []
+
+10.times {food << Item.create!(name:Faker::Commerce.color, price: Faker::Commerce.price)}
+
+4.times do
+	restaurant = restaurant_array.sample
+
+	menu = Menu.create!(name:Faker::Address.city,restaurant_id:restaurant.id)
+
+	10.times do
+		Pairing.create!(menu_id: menu.id,item_id: food.sample.id)
+	end
+
+end
