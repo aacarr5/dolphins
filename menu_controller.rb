582c7fbc9f056bbda97f4057eb84445482234799
Andrew Carr <aacarr5@gmail.com>
2015-10-20 16:52:07 -0500

    Create controller module

---
+++ menu_controller.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+module MenuController
+
+	def make_goal(line)
+		line[1..-1].to_f
+	end
+
+	def make_item(line)
+		line = line.gsub(/[\\\"\nn\$]/,"")
+		line = line.split(",")
+		Item.new(line[0],line[1].to_f)
+	end
+
+
+
+
+end
